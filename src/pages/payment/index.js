69a2d3cc038ed1c77e731ed64df18d5880877322
Dung Tram <thuydungtran2207@gmail.com>
2022-07-07 13:11:56 +0000

    add payment page

---
+++ src/pages/payment/index.js
@@ -0,0 +1,45 @@
+Page({
+  onLoad(query) {
+  },
+  onReady() {
+  },
+  onShow() {
+  },
+  onHide() {
+  },
+  onUnload() {
+  },
+
+  data: {
+    iconType: [
+      'home',
+      'search',
+      'calendar',
+      'location',
+      'setting',
+      'contact',
+      'chat',
+      'info',
+      'warning',
+      'success',
+      'plus',
+      'minus',
+      'geometry',
+      'close',
+      'close_circle',
+      'more',
+      'arrow_right',
+      'arrow_left',
+      'arrow_down',
+      'arrow_up',
+      'bookmark',
+      'app_home',
+      'phone_home',
+      'share',
+      'warning_glyph',
+      'success_glyph',
+      'close_glyph'
+    ]
+  }
+
+});
