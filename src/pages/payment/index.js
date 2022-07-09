0314d90a91d5e513efd8552c1597bb5cb4de28d9
Dung Tram <thuydungtran2207@gmail.com>
2022-07-09 17:26:53 +0000

    update payment

--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -11,35 +11,6 @@ Page({
   },
 
   data: {
-    iconType: [
-      'home',
-      'search',
-      'calendar',
-      'location',
-      'setting',
-      'contact',
-      'chat',
-      'info',
-      'warning',
-      'success',
-      'plus',
-      'minus',
-      'geometry',
-      'close',
-      'close_circle',
-      'more',
-      'arrow_right',
-      'arrow_left',
-      'arrow_down',
-      'arrow_up',
-      'bookmark',
-      'app_home',
-      'phone_home',
-      'share',
-      'warning_glyph',
-      'success_glyph',
-      'close_glyph'
-    ]
   }
 
 });
